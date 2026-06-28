3281ac628d3aa958b150b521dcf058e3a248b299
Jake Hope <jacob.hope@icloud.com>
2026-06-27 23:06:03 -0700

    First Commit

---
+++ TextGame1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TextGame1
+{
+    class Program
+    {
+        public static List<string> positiveAnswers = new List<string> { "yes", "yessir", "yes'm", "affirmative", "yeah", "ye", "sure", "i guess so", "sure why not", "certainly", "yesh", "of course", "decidedly so", "uh huh" };
+        public static List<string> negativeAnswers = new List<string> { "no", "nada", "negative", "three", "probably not", "no way", "certainly not", "yeah right", "absolutely not", "no way jose" };
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+            Thread.Sleep(1000);
+            Console.Write(".");
+            Thread.Sleep(100);
+            Console.Write(".");
+            Thread.Sleep(100);
+            Console.Write(".");
+            Thread.Sleep(100);
+            Console.Write("Should I call you Jimmy?");
+            string response = Console.ReadLine() ?? string.Empty;
+            
+            if (positiveAnswers.Contains(response))
+            {
+                Console.WriteLine("Excellent. That's what we like to hear!");
+            }
+            else if (positiveAnswers.Contains(response))
+            {
+                Console.WriteLine("Oh. Well that's too bad...");
+            }
+            else
+            {
+                Console.WriteLine("Hmmm. Well that's not the response I expected.");
+            }
+
+        }
+    }
+}
